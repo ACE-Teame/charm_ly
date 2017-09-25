08f2a83949fa1a7a9344af06bc928bee7d800d62
Iris88zjm <327055610@qq.com>
2017-09-25 15:03:06 +0800

    add telphone number

--- app/views/home/sidebar.php
+++ app/views/home/sidebar.php
@@ -1,12 +1,12 @@
 <footer>
-	<p>版权所有</p>
-	<p>ICP</p>
+	<p>技术支持 云遇工作室</p>
+	<p>粤ICP备16054473号-1</p>
 </footer>
 
 <div class="bottom">
 	<ul class="clear">
-		<li><a href="tel:8888888"><i class="iconfont icon-phone"></i><br/>电话咨询</a></li>
-		<li><a href="tel:8888888"><i class="iconfont icon-sms"></i><br/>信息咨询</a></li>
+		<li><a href="tel:13377774390"><i class="iconfont icon-phone"></i><br/>电话咨询</a></li>
+		<li><a href="sms:13377774390"><i class="iconfont icon-sms"></i><br/>信息咨询</a></li>
 		<li><a href="<?php echo base_url('index/board') ?>"><i class="iconfont icon-msg"></i><br/>在线留言</a></li>
 	</ul>
 </div>
